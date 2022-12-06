9ecb0e04df7df043f4bf661aefbfc27d752ad64f
Yanis <yagane33@gmail.com>
2022-12-05 23:20:22 -0500

    test

---
+++ test.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+net = cv.dnn.readNetFromTensorflow("graph_opt.pb")
+
+inWidth = 368
+inHeight = 368
+prob = 0.2
+
+BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                   "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                   "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Background": 14 }
+
+POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"]]
+
+def pose(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :15, :, :]
+
+    x = []
+    y = []
+    points = []
+    for i in range(len(BODY_PARTS)):
+        heatMap = out[0, i, :, :]
+
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+
+        x.append(50-point[0])
+        y.append(50-point[1])
+
+    return x,y
+
+img = cv.imread("image.jpg")
+
+x,y = pose(img)
+
+plt.plot(x,y,'.')
+
+plt.show()
